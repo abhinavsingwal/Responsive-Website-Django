fc61d6cdee2b27c23a4de833cca13227032ad2c8
abhinav singwal <abhinavsingwal@gmail.com>
2023-08-16 22:48:00 +0530

    Update settings.py

--- frontsilk/settings.py
+++ frontsilk/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abhinavsingwal@gmail.com'
-EMAIL_HOST_PASSWORD = 'thinkBig@7878'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
